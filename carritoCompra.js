c29712b2ba6f420696bee79c73304a35140b5a67
raulmunozs <ralexanderms@gmail.com>
2022-09-14 23:01:57 -0400

    carrito de compra

--- carritoCompra.js
+++ carritoCompra.js
@@ -1,4 +1,4 @@
-const cursos = [
+const productos = [
     {nombre: "lectura Rapida", precio: 300},
     {nombre: "Bullet Jurnal",  precio: 250},
     {nombre: "Economia para emprendedores", precio: 350}
@@ -6,27 +6,22 @@ const cursos = [
 
 let carrito = [];
 let seleccion = prompt("¿¡Listo para aprender?! \n si \n no");
-function inicio(){
-        while(seleccion != "si" && seleccion != "no"){
-            alert("Por favor indicar con un si o un no")
-            seleccion = prompt("Desea adquirir un curso \n Si \n No")
-       // init = parseInt(prompt("Que deseas aprender hoy? \n 1)Lectura Rapida. \n 2)Journaling. \n 3)Economía para emprendedores."))
-    } 
-    
-}
-inicio();
-productos();
-menu();
 
-function productos(){
-    if(seleccion =="si"){
-        alert("tenemos los siguientes cursos")
-        let todosCursos = cursos.map((cursos) => cursos.nombre + " " + cursos.precio + "$");
-        alert("Que deseas aprender hoy? \n 1)Lectura Rapida. \n 2)Journaling. \n 3)Economía para emprendedores. \n escriba el numero del curso")
-    } else if(seleccion =="no"){
-        alert("Nos vemos luego")
-    }
-}
+
+while(seleccion !== "si" && seleccion !== "no"){
+    alert("Por favor indicar con un si o un no")
+    seleccion = prompt("Desea adquirir un curso \n Si \n No")
+    } 
+if(seleccion =="si"){
+    alert("tenemos los siguientes cursos")
+    let todosProductos = productos.map((producto) => productos.nombre + " " + productos.precio + "$");
+    alert("Que deseas aprender hoy? \n 1)Lectura Rapida. \n 2)Journaling. \n 3)Economía para emprendedores. \n escriba el numero del curso")
+    } 
+    else if(seleccion =="no"){
+    alert("Nos vemos luego")
+        }
+
+
 
 function cantidades(){
     let unidades = parseInt(prompt("Cuantas personas haran el curso?"))
@@ -36,10 +31,12 @@ function cantidades(){
 
 function menu(){
     while(seleccion != "no"){
-        let producto = prompt("Por favor escribe el curso que deseas hacer")
+        let producto = parseInt(prompt("Por favor escribe el curso que deseas hacer"))
         let precio = 0
-        if(producto === 1 || producto === 2 || producto === 3){
-            switch(producto){
+
+        if(producto == "1" || producto == "2"|| producto == "3"){
+
+        switch(producto){
                 case 1:
                     precio = 300;
                     break;
@@ -53,9 +50,23 @@ function menu(){
                     alert("Porfavor ingresa un curso")
                     break;
                 }
-                cantidades();
-        } else {
-            alert("No tenemos ese producto")
+            let unidades = parseInt(prompt("cuantos serian?"))
+                carrito.push({producto,unidades, precio})
+                console.log(carrito)
+        } else{
+            alert("no tenemos ese producto")
         }
     }
+   seleccion = prompt("desea seguir comprando?")
+    while(seleccion ==="no"){
+        alert("gracias por la compra!")
+        carrito.forEach((carrito.Final) => {
+            console.log(`producto: ${carritoFinal.producto}, unidades: ${carrito.unidades},
+            total a pagar por producto: ${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+        break;
+    }
 }
+menu();
+
+
